751cbba4a2d55fd415e2d78dfc24bf246213b070
Jerry Busser <jerry.busser@gmail.com>
2016-09-02 09:22:17 -0400

    Add canonical-link attrs to raw_info hash

--- lib/omniauth/strategies/clever.rb
+++ lib/omniauth/strategies/clever.rb
@@ -8,12 +8,11 @@ module OmniAuth
       # are often initiated by Clever, not the client. When Clever initiates
       # login, a state parameter is not relevant nor sent.
 
-      option :name, "clever"
-      option :client_options, {
-        :site          => 'https://api.clever.com',
-        :authorize_url => 'https://clever.com/oauth/authorize',
-        :token_url     => 'https://clever.com/oauth/tokens'
-      }
+      option :name, 'clever'
+      option :client_options,
+        site: 'https://api.clever.com',
+        authorize_url: 'https://clever.com/oauth/authorize',
+        token_url: 'https://clever.com/oauth/tokens'
 
       # This option bubbles up to the OmniAuth::Strategies::OAuth2
       # when we call super in the callback_phase below.
@@ -22,7 +21,7 @@ module OmniAuth
 
       def token_params
         super.tap do |params|
-          params[:headers] = {'Authorization' => "Basic #{Base64.strict_encode64("#{options.client_id}:#{options.client_secret}")}"}
+          params[:headers] = { 'Authorization' => "Basic #{authz_header}" }
         end
       end
 
@@ -30,37 +29,68 @@ module OmniAuth
         error = request.params["error_reason"] || request.params["error"]
         stored_state = session.delete("omniauth.state")
         if error
-          fail!(error, CallbackError.new(request.params["error"], request.params["error_description"] || request.params["error_reason"], request.params["error_uri"]))
+          fail!(
+            error,
+            CallbackError.new(
+              request.params["error"],
+              request.params["error_description"] ||
+              request.params["error_reason"],
+              request.params["error_uri"]
+            )
+          )
         else
           # Only verify state if we've initiated login and have stored a state
           # to compare to.
-          if stored_state && (!request.params["state"] || request.params["state"] != stored_state)
-            fail!(:csrf_detected, CallbackError.new(:csrf_detected, "CSRF detected"))
+          if stored_state &&
+              (!request.params["state"] ||
+               request.params["state"] != stored_state)
+            fail!(
+              :csrf_detected,
+              CallbackError.new(:csrf_detected, "CSRF detected")
+            )
           else
             super
           end
         end
       end
 
-      uid{ raw_info['data']['id'] }
+      uid { me_attrs.fetch('data').fetch('id') }
 
       info do
-        { :user_type => raw_info['type'] }.merge! raw_info['data']
+        me_attrs.fetch('data')
       end
 
       extra do
         {
-          'raw_info' => raw_info
+          raw_info: me_attrs,
+          canonical: canonical_attrs
         }
       end
 
-      def raw_info
-        @raw_info ||= access_token.get('/me').parsed
+      def authz_header
+        Base64.strict_encode64(
+          "#{options.client_id}:#{options.client_secret}"
+        )
       end
 
-      # Fix unknown redirect uri bug by NOT appending the query string to the callback url.
+      def me_attrs
+        @me_attrs ||= access_token.get('/me').parsed
+      end
+
+      def canonical_attrs
+        @canonical_attrs ||= access_token.get(canonical_endpoint).parsed
+      end
+
+      def canonical_endpoint
+        me_link('canonical').fetch('uri', '/me')
+      end
+
+      def me_link(rel)
+        me_attrs.fetch('links', []).find { |link| link['rel'] == rel } || {}
+      end
+
       def callback_url
-        full_host + script_name + callback_path
+        "#{full_host}#{script_name}#{callback_path}"
       end
     end
   end
